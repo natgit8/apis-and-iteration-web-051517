3d5f2ff25f3c09463ec3a97413f379826303d430
Natasha Gitlin <natasha.gitlin8@gmail.com>
2017-05-17 10:47:49 -0400

    Finished command_line_interface

--- bin/run.rb
+++ bin/run.rb
@@ -1,8 +1,10 @@
 #!/usr/bin/env ruby
+require "pry"
 
 require_relative "../lib/api_communicator.rb"
 require_relative "../lib/command_line_interface.rb"
 
 welcome
 character = get_character_from_user
+binding.pry 
 show_character_movies(character)
